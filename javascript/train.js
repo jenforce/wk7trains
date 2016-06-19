924f83ac111cf978bbbff8bbea52fafd4a2ac84f
jenward <jenforce@aol.com>
2016-06-18 23:15:58 -0400

    sat updates duplicate row bug to fix css update needed

--- javascript/train.js
+++ javascript/train.js
@@ -1,83 +1,45 @@
 $(document).ready(function() {
-
-//var dataRef = new Firebase('https://employeepay7716.firebaseio.com/');
-
-var dataRef = new Firebase('https://popping-heat-7384.firebaseio.com/');
-var trainName = '';
-var destination = '';
-var trainStart = '';
-var frequency = '';
-
-console.log(trainName);
-
-$("#submitbutton").on("click",function(){
-debugger;
-	 trainName = $('#docTrain').val().trim(); 
-	 destination = $('#docDestination').val().trim();
-	 trainStart = $('#docStart').val().trim();
-	 frequency = $('#docFrequency').val().trim();
-
-	 console.log(trainName);
-	 logging();
-
-	dataRef.push({
-		trainName: trainName,
-		destination: destination,
-		trainStart: trainStart,
-		frequency: frequency
-	})
-});
-
-dataRef.on('child_added', function (childsnapshot){
-        $('#docTrain').html(childsnapshot.val().trainName);
-        $('#docDestination').html(childsnapshot.val().destination);
-        $('#docStart').html(childsnapshot.val().trainStart);
-        $('#docFrequency').html(childsnapshot.val().frequency);
-        });
-
-
-
-function logging(){
-	console.log(trainName +"js trainName");
-	console.log(destination + "js destination");
-	console.log(trainStart + "js trainStart");
-	console.log(frequency + "js frequency");
-	 
-}
-
-	
- });   //end - $(document).ready(function()
-
-/*
-
-
-var dataRef = new Firebase('https://jennew.firebaseio.com/');
-var name = '';
-var position = '';
-var beginDate = '';
-var rate = '';
-
-$("#submitbutton").on("click",function(){
-
-	 name = $('#employeeName').val().trim();
-	 position = $('#role').val().trim();
-	 beginDate = $('#startDate').val().trim();
-	 rate = $('#monthlyRate').val().trim();
-
-dataRef.push({
-	name: name,
-	position: position,
-	beginDate: Firebase.ServerValue.TIMESTAMP,
-	rate: rate
-})
-
-});
-
-dataRef.on('child_added', function (childsnapshot){
-        $('#employeeName').html(childsnapshot.val().name);
-        $('#role').html(childsnapshot.val().position);
-        $('#startDate').html(childsnapshot.val().beginDate);
-        $('#monthlyRate').html(childsnapshot.val().rate);
-        });
-
-*/
+	var dataRef = new Firebase('https://popping-heat-7384.firebaseio.com/');
+	var trainName = '';
+	var destination = '';
+	var trainStart = '';
+	var frequency = '';
+	console.log(trainName);
+	$('.btn').click(function(event) {
+		event.preventDefault()
+		trainName = $('#docTrain').val().trim();
+		destination = $('#docDestination').val().trim();
+		trainStart = $('#docStart').val().trim();
+		frequency = $('#docFrequency').val().trim();
+		dataRef.push({
+			trainName: trainName,
+			destination: destination,
+			trainStart: trainStart,
+			frequency: frequency
+		})
+	});
+	dataRef.on("child_added", function(childSnapshot) {
+		var train1Name = childSnapshot.val().trainName;
+		var train1Dest = childSnapshot.val().destination;
+		var train1Start = childSnapshot.val().trainStart;
+		var train1Freq = childSnapshot.val().frequency;
+		var tFrequency = train1Freq;
+		var firstTime = train1Start;
+		var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+		console.log(firstTimeConverted);
+		var currentTime = moment();
+		console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+		var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+		console.log("DIFFERENCE IN TIME: " + diffTime);
+		var tRemainder = diffTime % tFrequency;
+		console.log(tRemainder);
+		var tMinutesTillTrain = tFrequency - tRemainder;
+		console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+		// Next Train
+		var nextTrain = moment().add(tMinutesTillTrain, "minutes")
+		var ArrTime = moment(nextTrain).format("hh:mm")
+		$("tbody").append("<tr><td>" + train1Name + "</td><td>" + train1Dest +
+			"</td><td>" + train1Freq + "</td><td>" + ArrTime + "</td><td>" +
+			tMinutesTillTrain + "</td></tr>");
+	});
+}); //end - $(document).ready(function()
